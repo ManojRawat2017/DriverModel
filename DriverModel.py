7de50ef3542eac12415b978e4e0330c84db9130b
Jared Anderson <jared.bonjean.anderson@gmail.com>
2020-04-10 12:47:45 -0700

    add argument for datafile

--- DriverModel.py
+++ DriverModel.py
@@ -6,17 +6,27 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sys
 import os
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import scipy.stats as stats
 
-def rank_test(X):
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score, KFold, train_test_split, GridSearchCV, RandomizedSearchCV
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegressionCV, LinearRegression, LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+
+def prevent_singular_matrix(X):
     '''inputs: proposed feature matrix for a  regression model
     outputs: selection of redundant columns that will cause 
     proposed feature matrix X to produce an error if fit to y'''
     rank_test = pd.DataFrame(X.copy(),dtype=np.float64)
     for i in range(rank_test.shape[1]):
+        i = min(i,rank_test.shape[1])
         if i %50==0:
             print(round(i/rank_test.shape[1],1))
         df_to_rank = rank_test.iloc[:,:i+1]
@@ -25,7 +35,26 @@ def rank_test(X):
             print(i,np.linalg.matrix_rank(df_to_rank),problem_column)
             rank_test.drop(columns=problem_column,inplace=True)
 
-def make_driver_model(input_data)
+def make_scatter_matrix(X):
+    ''' optional EDA function for scatterplots '''
+    scatter_matrix = pd.plotting.scatter_matrix(df,figsize=(75,75),
+                              diagonal='kde',alpha=0.2)
+
+    [s.xaxis.label.set_rotation(300) for s in scatter_matrix.reshape(-1)]
+    [s.yaxis.label.set_rotation(60) for s in scatter_matrix.reshape(-1)]
+
+    #May need to offset label when rotating to prevent overlap of figure
+    [s.get_yaxis().set_label_coords(-0.3,0.5) for s in scatter_matrix.reshape(-1)]
+
+    [plt.setp(item.yaxis.get_label(), 'size', 20) for item in scatter_matrix.ravel()]
+    #x labels
+    [plt.setp(item.xaxis.get_label(), 'size', 20) for item in scatter_matrix.ravel()]
+
+    #Hide all ticks
+    # [s.set_xticks(()) for s in sm.reshape(-1)]
+    # [s.set_yticks(()) for s in sm.reshape(-1)]
+
+def make_driver_model(input_data):
     df = input_data.copy()
     df.sort_values(by='Feedback_DateTime',inplace=True)
     df.drop_duplicates(subset='Feedback_TenantId',keep='last',inplace=True)
@@ -50,7 +79,9 @@ def make_driver_model(input_data)
     time_to_drop = [#'Tenant_CompanyLastDirSyncTime','Tenant_PasswordSyncTime',
         'LastTicket_CreatedTime','LastTicket_ClosedTime'
                    ]
-    df.drop(columns=time_to_drop,inplace=True)
+    for x in time_to_drop:
+        if x in df.columns:
+            df.drop(columns=x,inplace=True)
     df = df.loc[:,~df.columns.str.contains('Date')]
 
     cats = df.columns[df.dtypes==object]
@@ -129,63 +160,44 @@ def make_driver_model(input_data)
     feature_cols = list(set(feature_cols))
 
     X_subset = X.loc[:,feature_cols].fillna(0)
-    rank_test = X_subset.copy()
-    linear_dependencies = []
-    for i in range(rank_test.shape[1]):
-        df_to_rank = rank_test.iloc[:,:i+1]
-        if i == np.linalg.matrix_rank(df_to_rank):
-            print(i,np.linalg.matrix_rank(df_to_rank))
-            problem_column = df_to_rank.columns[i]
-            linear_dependencies.append(problem_column)
-    print(linear_dependencies)
-    X_subset.drop(columns=linear_dependencies,inplace=True)
+#     rank_test = X_subset.copy()
+#     linear_dependencies = []
+#     for i in range(rank_test.shape[1]):
+#         df_to_rank = rank_test.iloc[:,:i+1]
+#         if i == np.linalg.matrix_rank(df_to_rank):
+#             print(i,np.linalg.matrix_rank(df_to_rank))
+#             problem_column = df_to_rank.columns[i]
+#             linear_dependencies.append(problem_column)
+#     print(linear_dependencies)
+#     X_subset.drop(columns=linear_dependencies,inplace=True)
 
     #redundant columns check
-    matrix = X_subset.corr()==1
-    linear_dependents = []
-    for col in matrix.columns:
-        #print(col)
-        linear_dependents.append(list(matrix[col].index[matrix[col]==True]))
-    print([x for x in linear_dependents if len(x)>1])
+#     for i in range(X_subset.shape[1]):
+#         if i < X_subset.shape[1]:
+#             if i %50==0:
+#                 print(round(i/X_subset.shape[1],1))
+#             df_to_rank = X_subset.iloc[:,:i+1]
+#             if i == np.linalg.matrix_rank(df_to_rank):
+#                 problem_column=df_to_rank.columns[i]
+#                 print(i,np.linalg.matrix_rank(df_to_rank),problem_column)
+#                 X_subset.drop(columns=problem_column,inplace=True)
 
-    for i in range(X_subset.shape[1]):
-        if i %50==0:
-            print(round(i/X_subset.shape[1],1))
-        df_to_rank = X_subset.iloc[:,:i+1]
-        if i == np.linalg.matrix_rank(df_to_rank):
-            problem_column=df_to_rank.columns[i]
-            print(i,np.linalg.matrix_rank(df_to_rank),problem_column)
-            X_subset.drop(columns=problem_column,inplace=True)
-
+    prevent_singular_matrix(X_subset)
 
     logit = sm.Logit(y, sm.add_constant(X_subset.T.drop_duplicates().T))
     flogit = logit.fit()
 
-    print(fmlogit.summary())
+    print(flogit.summary())
 
     coefficients = flogit.summary2().tables[1]
     coefficients = coefficients[coefficients['P>|z|']<0.1]
     coefficients['FinalProbability'] = np.exp(coefficients['Coef.'].round(1))*y.value_counts(normalize=True).loc[1] #- 0.5
     coefficients['FinalProbability'] = coefficients['FinalProbability'].mask(coefficients['FinalProbability']>=1,0.99)
     coefficients['Lift'] = coefficients['FinalProbability'] - y.value_counts(normalize=True).loc[1]
-    coefficients['Model'] = name
-    coef_dict[name] = coefficients
+    return flogit, coefficients
 
-def make_scatter_matrix(df):
-    scatter_matrix = pd.plotting.scatter_matrix(df,figsize=(75,75),
-                              diagonal='kde',alpha=0.2)
+df = sys.argv[1] #point to the data file you are using in terminal
+model, scorecard = make_driver_model(df)
+scorecard.to_csv('DriverModelScorecard.csv')
 
-    [s.xaxis.label.set_rotation(300) for s in scatter_matrix.reshape(-1)]
-    [s.yaxis.label.set_rotation(60) for s in scatter_matrix.reshape(-1)]
-
-    #May need to offset label when rotating to prevent overlap of figure
-    [s.get_yaxis().set_label_coords(-0.3,0.5) for s in scatter_matrix.reshape(-1)]
-
-    [plt.setp(item.yaxis.get_label(), 'size', 20) for item in scatter_matrix.ravel()]
-    #x labels
-    [plt.setp(item.xaxis.get_label(), 'size', 20) for item in scatter_matrix.ravel()]
-
-    #Hide all ticks
-    # [s.set_xticks(()) for s in sm.reshape(-1)]
-    # [s.set_yticks(()) for s in sm.reshape(-1)]
 
